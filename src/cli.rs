ffd3360fe5986d9f7b230c0e168a50a8dc33ba11
Bryant Biggs <bryantbiggs@gmail.com>
2024-10-22 10:47:32 -0500

    chore: Init to winit :fire:

---
+++ src/cli.rs
@@ -0,0 +1,53 @@
+use std::path::PathBuf;
+
+use anstyle::{AnsiColor, Color, Style};
+use anyhow::Result;
+use clap::{builder::Styles, Parser};
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+
+/// Styles for CLI
+fn get_styles() -> Styles {
+  Styles::styled()
+    .header(
+      Style::new()
+        .bold()
+        .underline()
+        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
+    )
+    .literal(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+    .usage(
+      Style::new()
+        .bold()
+        .underline()
+        .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
+    )
+    .placeholder(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Magenta))))
+}
+
+/// k8sfg - A CLI to display feature gates across Kubernetes versions.
+///
+/// ToDo
+#[derive(Debug, Parser)]
+#[command(author, about, version)]
+#[command(propagate_version = true)]
+#[command(styles=get_styles())]
+pub struct Cli {
+  /// The path to write the collected results to
+  #[clap(short, long, default_value = "RESULTS.md")]
+  pub path: PathBuf,
+
+  #[clap(flatten)]
+  pub verbose: Verbosity<InfoLevel>,
+}
+
+impl Cli {
+  fn _collect(self) -> Result<()> {
+    Ok(())
+  }
+
+  pub fn write(self) -> Result<()> {
+    // let data = self.collect()?;
+    // fs::write(Path::new(&self.path), data).map_err(Into::into)
+    Ok(())
+  }
+}
