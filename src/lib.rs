6947d0d76e34b0a0f2163647b9b6ce265cb51644
Bryant Biggs <bryantbiggs@gmail.com>
2024-11-04 12:31:02 -0600

    feat: Collect feature gate data in denormalized table and display

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,83 @@
 pub mod cli;
 
+use std::collections::{HashMap, HashSet};
+
 use anyhow::{bail, Result};
 pub use cli::Cli;
+use json_to_table::json_to_table;
 use once_cell::sync::Lazy;
 use regex_lite::Regex;
+use serde_json::{json, Value};
+use tempfile::{tempdir, TempDir};
 use tokio::io::AsyncWriteExt;
 use tracing::info;
 
-// curl https://cdn.dl.k8s.io/release/stable-1.29.txt
+static K8S_MINOR_VERSIONS: &[i32] = &[27, 28, 29, 30, 31];
+static K8S_BINARIES: &[&str] = &[
+  "kube-apiserver",
+  "kubelet",
+  "kube-controller-manager",
+  "kube-proxy",
+  "kube-scheduler",
+];
 
-pub async fn collect_feature_gates(client: reqwest::Client) -> Result<()> {
-  let version = "v1.29.10";
-  let binary = "kubelet";
+type KubernetesVersion = String;
+type FeatureGates = Vec<FeatureGate>;
+type FeatureGateNames = HashSet<String>;
+type FeatureGateData = HashMap<KubernetesVersion, FeatureGates>;
 
-  let bin = download_binary(client, version, binary).await?;
-  let content = get_binary_output(&bin)?;
-  let feature_gates = extract_feature_gates(&content)?;
+pub async fn display_feature_gates(client: reqwest::Client) -> Result<()> {
+  let (fg_data, fg_names) = collect_feature_gates(client).await?;
+  let results = merge_feature_gates(fg_data, fg_names)?;
 
-  println!("Feature Gates: {:#?}", feature_gates);
+  let json = serde_json::to_value(results)?;
+  let table = json_to_table(&json).to_string();
+  println!("{}", table);
 
   Ok(())
+}
+
+async fn collect_feature_gates(client: reqwest::Client) -> Result<(FeatureGateData, FeatureGateNames)> {
+  let tmp = tempdir()?;
+  let mut data: FeatureGateData = HashMap::new();
+  let mut names: FeatureGateNames = HashSet::new();
+
+  for minor_version in K8S_MINOR_VERSIONS {
+    let full_version = client
+      .get(format!("https://cdn.dl.k8s.io/release/stable-1.{minor_version}.txt"))
+      .send()
+      .await?
+      .text()
+      .await?;
+
+    for binary in K8S_BINARIES {
+      let bin = download_binary(&client, &tmp, &full_version, binary).await?;
+      let content = get_binary_output(&bin)?;
+      let (fgs, fg_names) = extract_feature_gates(&content)?;
+
+      data.insert(full_version.to_owned(), fgs);
+      names.extend(fg_names);
+    }
+  }
+
+  Ok((data, names))
+}
+
+fn merge_feature_gates(fg_data: FeatureGateData, fg_names: FeatureGateNames) -> Result<Vec<Value>> {
+  let mut results = vec![];
+
+  for name in fg_names {
+    for (k8s_version, feature_gates) in fg_data.iter() {
+      for fg in feature_gates {
+        results.push(json!({
+          "Feature Gate Name": name,
+          k8s_version: format!("{} - {}", fg.level, fg.default),
+        }));
+      }
+    }
+  }
+
+  Ok(results)
 }
 
 // MacOs is not supported !!!!
@@ -40,13 +98,12 @@ fn get_url(version: &str, binary: &str) -> Result<String> {
   Ok(format!("https://dl.k8s.io/{version}/bin/{os}/{arch}/{binary}"))
 }
 
-async fn download_binary(client: reqwest::Client, version: &str, binary: &str) -> Result<String> {
+async fn download_binary(client: &reqwest::Client, tmp: &TempDir, version: &str, binary: &str) -> Result<String> {
   let url = get_url(version, binary)?;
   info!("Download URL: {url}");
   let mut response = client.get(url).send().await?;
 
-  let bin_dir = std::env::current_dir()?;
-  let fname = bin_dir.join(binary);
+  let fname = tmp.path().join(binary);
 
   let mut dest = tokio::fs::OpenOptions::new()
     .truncate(true)
@@ -73,7 +130,6 @@ fn get_binary_output(binary: &str) -> Result<String> {
     .spawn()?;
 
   let result = proc.wait_with_output()?;
-  info!("Result: {:#?}", result);
   let out = String::from_utf8_lossy(&result.stdout).to_string();
 
   Ok(out)
@@ -115,17 +171,19 @@ struct FeatureGate {
   default: bool,
 }
 
-fn extract_feature_gates(content: &str) -> Result<Vec<FeatureGate>> {
+fn extract_feature_gates(content: &str) -> Result<(FeatureGates, FeatureGateNames)> {
   static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(.*?)=.*?\((ALPHA|BETA|GA).*default=(true|false)").unwrap());
 
-  let mut results = vec![];
+  let mut gates: Vec<_> = vec![];
   for (_, [name, level, default]) in RE.captures_iter(content).map(|c| c.extract()) {
-    results.push(FeatureGate {
+    gates.push(FeatureGate {
       name: name.trim().to_string(),
       level: level.into(),
       default: default.parse()?,
     })
   }
 
-  Ok(results)
+  let names = gates.iter().map(|fg| fg.name.clone()).collect::<HashSet<String>>();
+
+  Ok((gates, names))
 }
